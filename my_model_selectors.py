8ed21755f492fe6c04267d330f7f3d1e0daa29a4
Alex X <alex.o@dynamic2.ro>
2017-05-24 19:00:12 +0300

    pass asl_test_model_selectors

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -58,6 +58,7 @@ class SelectorConstant(ModelSelector):
         :return: GaussianHMM object
         """
         best_num_components = self.n_constant
+        # return False
         return self.base_model(best_num_components)
 
 
@@ -76,8 +77,47 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        word = self.this_word
+        word_sequences = self.sequences
+        if self.verbose:
+            # print(f"My sequences for {word}: {self.sequences}")
+            pass
+        
+        hmm_models = {}
+        best_score = None
+        best_n = None
+        if self.verbose:
+            print(f"trying to build models with {self.min_n_components} -> {self.max_n_components} states")
+        for n in range(self.min_n_components, self.max_n_components):
+            # print(f"{n} components")
+            try:
+                model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+                hmm_models[n] = model
+                # evaluate the model
+                logL = model.score(self.X, self.lengths)
+                # print(f"means:\n{model.means_}")
+                # print(f"covars:\n{model.covars_}") 
+                parameters_count = n * n + 2*n*len(self.X[0]) -1
+                logN = np.log(len(self.lengths))
+                bic = -2 * logL + parameters_count * logN
+                if best_score is None or best_score < bic :
+                    best_score = bic
+                    best_n = n
+                if self.verbose:
+                    print("model created for {} with {} states. score={}".format(word, n, logL))
+            except ValueError:
+                hmm_models[n] = None
+                if self.verbose:
+                    print(f"failed to build model with {n} states for {word}")
+               
+        # acum cred ca trebuie sa le evaluez si sa aleg pe cel mai bun
+
+        if self.verbose:
+            print(f"best model for {word} has {best_n} states")
+        if best_n is None:
+            return None
+        return hmm_models[best_n]
 
 
 class SelectorDIC(ModelSelector):
@@ -92,8 +132,62 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        word = self.this_word
+        word_sequences = self.sequences
+        if self.verbose:
+            print(f"My sequences for {word}: {self.sequences}")
+        
+        hmm_models = {}
+        best_score = None
+        best_n = None
+        if self.verbose:
+            print(f"trying to build models with {self.min_n_components} -> {self.max_n_components} states")
+        for n in range(self.min_n_components, self.max_n_components):
+            # print(f"{n} components")
+            try:
+                model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+                hmm_models[n] = model
+                # evaluate the model
+                logL = model.score(self.X, self.lengths)
+                
+                sum_logL = 0
+                count_logs = 0
+                #go through all the other words and calculate logL for each, add it to sum_logL
+                for other_word in self.hwords.keys():
+                    if other_word == word:
+                        continue
+                    
+                    try:
+                        other_X, other_lengths = self.hwords[other_word]
+                        logL = model.score(other_X, other_lengths) / len(other_lengths)
+                        sum_logL += logL
+                        count_logs +=1
+                    except:
+                        # could not score this model for other_word
+                        if self.verbose:
+                            print(f"could not score {word} model with {n} states on {other_word}")
+                
+                dic = logL
+                if count_logs:
+                    dic -= sum_logL / count_logs
+                if best_score is None or best_score < dic :
+                    best_score = dic
+                    best_n = n
+                if self.verbose:
+                    print("model created for {} with {} states. score={}".format(word, n, logL))
+            except ValueError:
+                hmm_models[n] = None
+                if self.verbose:
+                    print(f"failed to build model with {n} states for {word}")
+               
+        # acum cred ca trebuie sa le evaluez si sa aleg pe cel mai bun
+
+        if self.verbose:
+            print(f"best model for {word} has {best_n} states")
+        if best_n is None:
+            return None
+        return hmm_models[best_n]
 
 
 class SelectorCV(ModelSelector):
@@ -102,7 +196,49 @@ class SelectorCV(ModelSelector):
     '''
 
     def select(self):
+        # return True
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        word = self.this_word
+        word_sequences = self.sequences
+        if self.verbose:
+            print(f"My sequences for {word}: {self.sequences}")
+        # split_method = KFold()
+        split_method = KFold(n_splits=min(3,len(self.lengths)))
+        # nu stiu daca am nevoie de toate combinatiile (cv_train_idx, cv_test_idx)
+        cv_train_idx, cv_test_idx = next(split_method.split(word_sequences))
+        # print("Train fold indices:{} Test fold indices:{}".format(cv_train_idx, cv_test_idx))  # view indices of the folds
+        X, lengths = combine_sequences(cv_train_idx, word_sequences)
+        Y, y_lengths = combine_sequences(cv_test_idx, word_sequences)
+        # print("X: {}".format(X))
+        # print("lengths: {}".format(lengths))
+        
+        hmm_models = {}
+        best_score = None
+        best_n = None
+        if self.verbose:
+            print(f"trying to build models with {self.min_n_components} -> {self.max_n_components} states")
+        for n in range(self.min_n_components, self.max_n_components):
+            # print(f"{n} components")
+            try:
+                model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(X, lengths)
+                hmm_models[n] = model
+                # ToDo
+                # evaluez modelul dar nu pe X si lengths ( care sunt Train indices), ci pe Test Indices
+                logL = model.score(Y, y_lengths)
+                if best_score is None or best_score < logL :
+                    best_score = logL
+                    best_n = n
+                if self.verbose:
+                    print("model created for {} with {} states. score={}".format(word, n, logL))
+            except ValueError:
+                hmm_models[n] = None
+                if self.verbose:
+                    print(f"failed to build model with {n} states for {word}")
+               
+        # acum cred ca trebuie sa le evaluez si sa aleg pe cel mai bun
+
+        if self.verbose:
+            print(f"best model for {word} has {best_n} states")
+        return hmm_models[best_n]
