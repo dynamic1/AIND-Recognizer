442c57257515de07322f1d0bf39f6693721b0a80
Alex X <alex.o@dynamic2.ro>
2017-05-26 01:34:23 +0300

    fixed CV k splits and BIC selection

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -84,7 +84,7 @@ class SelectorBIC(ModelSelector):
             pass
         
         hmm_models = {}
-        best_score = None
+        best_score = math.inf
         best_n = None
         if self.verbose:
             print(f"trying to build models with {self.min_n_components} -> {self.max_n_components} states")
@@ -99,9 +99,11 @@ class SelectorBIC(ModelSelector):
                 # print(f"means:\n{model.means_}")
                 # print(f"covars:\n{model.covars_}") 
                 parameters_count = n * n + 2*n*len(self.X[0]) -1
+                # parameters_count = model.startprob_.size + model.transmat_.size \
+                                    # + model.means_.size + model.covars_.diagonal().size
                 logN = np.log(len(self.lengths))
                 bic = -2 * logL + parameters_count * logN
-                if best_score is None or best_score < bic :
+                if best_score is None or best_score > bic :
                     best_score = bic
                     best_n = n
                 if self.verbose:
@@ -110,6 +112,7 @@ class SelectorBIC(ModelSelector):
                 hmm_models[n] = None
                 if self.verbose:
                     print(f"failed to build model with {n} states for {word}")
+                
                
         # acum cred ca trebuie sa le evaluez si sa aleg pe cel mai bun
 
@@ -135,7 +138,8 @@ class SelectorDIC(ModelSelector):
         word = self.this_word
         word_sequences = self.sequences
         if self.verbose:
-            print(f"My sequences for {word}: {self.sequences}")
+            # print(f"My sequences for {word}: {self.sequences}")
+            pass
         
         hmm_models = {}
         best_score = None
@@ -154,6 +158,10 @@ class SelectorDIC(ModelSelector):
                 sum_logL = 0
                 count_logs = 0
                 #go through all the other words and calculate logL for each, add it to sum_logL
+                # other_words_penalty = np.mean( [model.score(self.hwords[other_word]) \
+                                               # for other_word in self.words if other_word != word ])
+                # dic = logL - other_words_penalty
+                
                 for other_word in self.hwords.keys():
                     if other_word == word:
                         continue
@@ -171,6 +179,7 @@ class SelectorDIC(ModelSelector):
                 dic = logL
                 if count_logs:
                     dic -= sum_logL / count_logs
+                
                 if best_score is None or best_score < dic :
                     best_score = dic
                     best_n = n
@@ -180,6 +189,7 @@ class SelectorDIC(ModelSelector):
                 hmm_models[n] = None
                 if self.verbose:
                     print(f"failed to build model with {n} states for {word}")
+                # raise
                
         # acum cred ca trebuie sa le evaluez si sa aleg pe cel mai bun
 
@@ -199,6 +209,17 @@ class SelectorCV(ModelSelector):
         # return True
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+        """
+        Score on all folds for every n, and take the average. 
+            The idea of CV is to split the data into K-folds, 
+        each fold containing a training split and a testing split.
+            For each value of the number of components, 
+        you iterate and find the average log likelihood over the folds. 
+            This is done by fitting on the training split and scoring 
+        on the testing split for each fold, and then averaging over all folds.
+            Then when you have an average likelihood for each value of 
+        the number of components, you return the best one.
+        """
         word = self.this_word
         word_sequences = self.sequences
         if self.verbose:
@@ -208,40 +229,68 @@ class SelectorCV(ModelSelector):
             if self.verbose:
                 print(f"not enough sequences ( {self.sequences} ) to train / crossvalidate {word}") 
             return None
-        split_method = KFold(n_splits=min(3,len(self.lengths)))
-        
-        # nu stiu daca am nevoie de toate combinatiile (cv_train_idx, cv_test_idx)
-        cv_train_idx, cv_test_idx = next(split_method.split(word_sequences))
-        # print("Train fold indices:{} Test fold indices:{}".format(cv_train_idx, cv_test_idx))  # view indices of the folds
-        X, lengths = combine_sequences(cv_train_idx, word_sequences)
-        Y, y_lengths = combine_sequences(cv_test_idx, word_sequences)
-        # print("X: {}".format(X))
-        # print("lengths: {}".format(lengths))
         
         hmm_models = {}
-        best_score = None
+        
+        best_score_overall = None
         best_n = None
-        if self.verbose:
-            print(f"trying to build models with {self.min_n_components} -> {self.max_n_components} states")
+        fold = 0
+        
+        n_fold_model_score = []
         for n in range(self.min_n_components, self.max_n_components):
-            # print(f"{n} components")
-            try:
-                model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
-                                    random_state=self.random_state, verbose=False).fit(X, lengths)
-                hmm_models[n] = model
+            
+            models_for_n = 0
+            sum_logL_for_n = 0  
+            best_score_for_n = None
+            best_model_for_n = None
+            
+            # generate folds for training / testing
+            fold = 0
+            split_method = KFold(n_splits=min(3,len(self.lengths)))
+            for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
                 
-                # evaluez model
-                logL = model.score(Y, y_lengths)
-                if best_score is None or best_score < logL :
-                    best_score = logL
-                    best_n = n
+                fold += 1
                 if self.verbose:
-                    print("model created for {} with {} states. score={}".format(word, n, logL))
-            except ValueError:
-                hmm_models[n] = None
+                    # print(f"trying to build model with {self.min_n_components} -> {self.max_n_components} states: fold={fold}")
+                    pass
+
+                # build training and testing data
+                X, lengths = combine_sequences(cv_train_idx, word_sequences)
+                Y, y_lengths = combine_sequences(cv_test_idx, word_sequences)
+            
+                # try to train a model on the training split
+                try:
+                    model = GaussianHMM(n_components=n, covariance_type="diag", n_iter=1000,
+                                random_state=self.random_state, verbose=False).fit(X, lengths)
+                    models_for_n += 1
+                    # evaluate the model on the testing split
+                    logL = model.score(Y, y_lengths)
+                    sum_logL_for_n += logL
+                    if best_score_for_n is None or best_score_for_n < logL :
+                        best_score_for_n = logL
+                        best_model_for_n = model
+                        
+                    if self.verbose:
+                        print(f"model created for {word} with {n} states. fold={fold}, score={logL}")
+                except ValueError:
+                     if self.verbose:
+                        print(f"failed to build model for {word} with {n} states in fold {fold}")
+                                        
+            if not models_for_n:
                 if self.verbose:
-                    print(f"failed to build model with {n} states for {word}")
-       
+                    print(f"no models generated for {word} with {n} states")
+                continue
+                    
+            hmm_models[n] = best_model_for_n           
+            # now average the scores for all the folds of this n
+            avg_logL_for_n = sum_logL_for_n / models_for_n
+            if self.verbose:
+                print(f"best model for {word} whith {n} states scores {best_score_for_n}")
+                print(f"average score for {word} with {n} states is {avg_logL_for_n}")
+            if best_score_overall is None or best_score_overall < avg_logL_for_n :
+                best_score_overall = avg_logL_for_n
+                best_n = n   
+                
         if self.verbose:
             print(f"best model for {word} has {best_n} states")
         if best_n is None:
